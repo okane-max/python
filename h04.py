c0060d91a46fea08d3a9f194c3c36192699656be
okane-max <okane@hkhk.edu.ee>
2025-10-11 12:09:43 +0300

    Update h04.py
    harjutus04 korrastus

--- h04.py
+++ h04.py
@@ -1,38 +1,39 @@
-#harjutus4_5
 import turtle
-try:
-    raadius = int(input("Sisestage ringi raadius: "))
-    pindala = (raadius**2)*3.14
-    umbermoot = 2*raadius*3.14
-    print(f"Ringi pindala on {pindala:0.2f} ja ümbermõõt on {umbermoot:0.2f}")
-    turtle.circle(raadius,360)
-    turtle.done()
-except:
-    print("Kontrolli sisestust!")
+
+def harjutus4_1():
+    a = int(input("Lisa külg 1: "))
+    b = int(input("Lisa külg 2: "))
+
+    p = (a+b)*2
+    print(f"Aia kogupikkus külgedega {a} ja {b} on {p} meetrit.")
 
 
-#harjutus4_4
-try:
-    maht = 5
-    kingitused = int(input("Lisa kingituste arv: "))
-    kinkekarbid = int(kingitused/maht)
-    ulejaak = kingitused%maht
-    print("Saad teha " + str(kinkekarbid) + " täis kinkekasti. Üle jääb " + str(ulejaak) + "kingitust.")
-except:
-    print("Kontrolli sisestust!")
+def harjutus4_2():
+    ale = 0.3
+    hind = 18.50
+    kogus = int(input("Sisesta raamatute kogus: "))
+    summa = (hind-hind*ale)*kogus
+    print(f"{kogus} raamatu hind soodustusega on {summa:0.2f}€.")
 
 
-#harjutus4_2
-ale = 0.3
-hind = 18.50
-kogus = int(input("Sisesta raamatute kogus: "))
-summa = (hind-hind*ale)*kogus
-print(f"{kogus} raamatu hind soodustusega on {summa:0.2f}€.")
+def harjutus4_4():
+    try:
+        maht = 5
+        kingitused = int(input("Lisa kingituste arv: "))
+        kinkekarbid = int(kingitused/maht)
+        ulejaak = kingitused%maht
+        print("Saad teha " + str(kinkekarbid) + " täis kinkekasti. Üle jääb " + str(ulejaak) + "kingitust.")
+    except:
+        print("Kontrolli sisestust!")
 
 
-#harjutus4_1
-a = int(input("Lisa külg 1: "))
-b = int(input("Lisa külg 2: "))
-
-p = (a+b)*2
-print(f"Aia kogupikkus külgedega {a} ja {b} on {p} meetrit.")
+def harjutus4_5():
+    try:
+        raadius = int(input("Sisestage ringi raadius: "))
+        pindala = (raadius**2)*3.14
+        umbermoot = 2*raadius*3.14
+        print(f"Ringi pindala on {pindala:0.2f} ja ümbermõõt on {umbermoot:0.2f}")
+        turtle.circle(raadius,360)
+        turtle.done()
+    except:
+        print("Kontrolli sisestust!")
