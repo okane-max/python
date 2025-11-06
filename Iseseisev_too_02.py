2719acddcf5a2b13e982c73126126a68105056be
okane-max <okane@hkhk.edu.ee>
2025-11-06 13:03:02 +0200

    skaleeritav

--- Iseseisev_too_02.py
+++ Iseseisev_too_02.py
@@ -1,44 +1,44 @@
 import turtle
 
-def uks(x,y,korgus):
+def uks(x,y,pikkus,korgus):
     turtle.penup()
-    turtle.goto(x+25,y)
+    turtle.goto(x+(pikkus/4),y)
     turtle.pendown()
-    if korgus > 50:
+    if korgus > pikkus/2:
         turtle.color("blue")
     else:
         turtle.color("red")
     for i in range(2):
-        turtle.fd(50)
+        turtle.fd(pikkus/2)
         turtle.lt(90)
         turtle.fd(korgus)
         turtle.lt(90)
 
-def katus(x,y):
+def katus(x,y,pikkus):
     turtle.penup()
-    turtle.goto(x,y+100)
+    turtle.goto(x,y+pikkus)
     turtle.pendown()
     turtle.color("green")
     for i in range(3):
-        turtle.fd(100)
+        turtle.fd(pikkus)
         turtle.left(120)
 
-def seinad(x,y):
+def seinad(x,y,pikkus):
     turtle.penup()
     turtle.goto(x,y)
     turtle.pendown()
     turtle.color("black")
     for i in range(4):
-        turtle.fd(100)
+        turtle.fd(pikkus)
         turtle.lt(90)
 
-def maja(x,y,ukse_korgus):
-    uks(x,y,ukse_korgus)
-    katus(x,y)
-    seinad(x,y)
+def maja(x,y,pikkus,ukse_korgus):
+    uks(x,y,pikkus,ukse_korgus)
+    katus(x,y,pikkus)
+    seinad(x,y,pikkus)
 
-
-x = 125
+pikkus = 175
+x = pikkus+pikkus/4
 y = 0
 
 turtle.speed(0)
@@ -46,9 +46,9 @@ turtle.width(2)
 
 for i in range(10):
     if i < 5:
-        maja(x*i,y,50)
+        maja(x*i,y,pikkus,pikkus/2)
     else:
-        maja(x*(i-5),y-225,75)
+        maja(x*(i-5),y-(pikkus*2+pikkus/4),pikkus,((pikkus/4)*3))
 
 turtle.ht()
 turtle.done()
