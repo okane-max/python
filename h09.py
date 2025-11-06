28772df9475687fc2b5cee9f970f82f21115437a
okane-max <okane@hkhk.edu.ee>
2025-11-06 12:24:55 +0200

    harjutus9

--- h09.py
+++ h09.py
@@ -36,3 +36,43 @@ def harjutus9_5():
         if i%7 == 0 and i%5 != 0:
             arvud += (str(i) + ",")
     print(arvud)
+
+    #Harjutus09_13
+def harjutus9_13():
+    ev_data = [
+    ['vehicle', 'range', 'price'],
+    ['Tesla Model Y Long Range', '330', '58990'],
+    ['Volkswagen ID.4 Pro', '260', '39995'],
+    ['Ford Mustang Mach-E', '300', '42995'],
+    ['Audi e-tron GT', '238', '102700'],
+    ['Nissan Leaf', '149', '27400'],
+    ['BMW iX xDrive50', '324', '83995'],
+    ['Polestar 2', '265', '45500'],
+    ['Kia EV6 Long Range', '310', '47795'],
+    ['Mercedes-Benz EQS 450+', '350', '102310'],
+    ['Hyundai Kona Electric', '258', '37400']
+    ]
+    suurem = []
+
+    u_kokku = 0
+    h_kokku = 0
+    parim_hind = 1000
+    parim_auto = ""
+
+    for i in ev_data:
+        print(f"{i[0]:30} {i[1]:10} {i[2]:10}")
+        if i[1].isnumeric() == True:
+            km_tasu = int(i[2])/int(i[1])
+            if km_tasu < parim_hind:
+                parim_hind = km_tasu
+                parim_auto = i[0]
+            u_kokku += int(i[1])
+            h_kokku += int(i[2])
+            if int(i[1]) > 300:
+                suurem.append(i[0])
+    print("Keskmine ulatus: " + str(u_kokku/10) +"\n"+ "Keskmine hind: " + str(h_kokku/10))
+    print("Läbisõidu ulatus on suurem kui 300 km")
+    for i in suurem:
+        print(i)
+    print("Parima hinnaga auto: " + str(parim_auto))
+harjutus9_13()
